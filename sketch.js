8db12fea8a55cdbeac983551cc44e7798dc1d5ca
coder123-project <83872425+coder123-project@users.noreply.github.com>
2022-10-15 06:46:29 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,56 @@
+var bg,bgImg;
+var player, shooterImg, shooter_shooting;
+
+
+//loadImages for player, background and shooter player 
+
+
+
+
+
+function setup() {
+
+  
+  createCanvas(windowWidth,windowHeight);
+
+  //adding the background image
+
+
+//creating the player sprite
+
+  //player.debug = true
+   // player.debug = false
+    // player.Debug =false
+    // Player.debug = true
+
+   //player.Collider("rectagle",0,0,300,300)
+   //player.setcollider("rectangle",0,0)
+  // player.setCollider("rectangle",0,0,300,300)
+  // player.Setcollider("rectangle",0,0,300,300)
+
+}
+
+function draw() {
+  background(0); 
+
+
+
+
+  //moving the player up and down and making the game mobile compatible using touches
+
+
+
+//release bullets and change the image of shooter to shooting position when space is pressed
+
+//player goes back to original standing image once we stop pressing the space bar
+else if(keyDown("space")){
+  //player.addImage( shooter_shooting )
+ // player.addImage()
+ // player.addImage(shooterImg)
+ //player.addImage(shooter_1.png)
+
+}
+
+drawSprites();
+
+}
